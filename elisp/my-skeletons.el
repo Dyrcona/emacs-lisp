5443b9d9ae7fdc8b01efed69aff1c8268fc4922f
Jason Stephenson <jason@sigio.com>
2022-11-08 08:00:02 -0500

    Improve my-skeletons
    Eliminate a warning about skeleton-positions being a free variable by
    requiring the skeleton feature.
    
    Also, remove the "earmuffs" from the skeleton-markers variable.
    That's a habit from Common Lisp that isn't very common in Emacs Lisp.

--- elisp/my-skeletons.el
+++ elisp/my-skeletons.el
@@ -14,25 +14,26 @@
 
 ;; A library of my skeletons.
 
+(require 'skeleton)
 ;; First, some code stolen from EmacsWiki
 ;; (https://www.emacswiki.org/emacs/SkeletonMode#h5o-15) to make
 ;; navigating in skeletons easier.
-(defvar *skeleton-markers* nil
+(defvar skeleton-markers nil
   "Markers for locations saved in skeleton-positions")
 
 (add-hook 'skeleton-end-hook 'skeleton-make-markers)
 
 (defun skeleton-make-markers ()
-  (while *skeleton-markers*
-    (set-marker (pop *skeleton-markers*) nil))
-  (setq *skeleton-markers*
+  (while skeleton-markers
+    (set-marker (pop skeleton-markers) nil))
+  (setq skeleton-markers
         (mapcar 'copy-marker (reverse skeleton-positions))))
 
 (defun skeleton-next-position (&optional reverse)
   "Jump to next position in skeleton.
          REVERSE - Jump to previous position in skeleton"
   (interactive "P")
-  (let* ((positions (mapcar 'marker-position *skeleton-markers*))
+  (let* ((positions (mapcar 'marker-position skeleton-markers))
          (positions (if reverse (reverse positions) positions))
          (comp (if reverse '> '<))
          pos)
@@ -43,7 +44,7 @@
                 (throw 'break t))))
           (goto-char pos)
         (goto-char (marker-position
-                    (car *skeleton-markers*)))))))
+                    (car skeleton-markers)))))))
 ;; End of stolen code.
 
 ;; Use C-% for 'skeleton-next-position.
