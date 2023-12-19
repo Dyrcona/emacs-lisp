1884492bf45c91394383bf3d1014ca90a979599f
Jason Stephenson <jason@sigio.com>
2023-12-19 13:45:20 -0500

    Use PSR2 coding style for PHP on ils-mgr

--- ils-mgr-custom.el
+++ ils-mgr-custom.el
@@ -17,6 +17,8 @@
 ;; Set up for CW MARS
 (require 'cwmars)
 
+(add-hook 'php-mode-hook 'php-enable-psr2-coding-style)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
