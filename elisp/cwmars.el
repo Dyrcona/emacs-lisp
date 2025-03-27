a55ac53a6e369da746687d498040ce9c45344572
Jason Stephenson <jason@sigio.com>
2025-03-27 10:38:58 -0400

    Update copyright in elisp/cwmars.el

--- elisp/cwmars.el
+++ elisp/cwmars.el
@@ -1,5 +1,5 @@
 ;; ---------------------------------------------------------------
-;; Copyright © 2022, 2023 Jason J.A. Stephenson <jason@sigio.com>
+;; Copyright © 2022-2025 Jason J.A. Stephenson <jason@sigio.com>
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
